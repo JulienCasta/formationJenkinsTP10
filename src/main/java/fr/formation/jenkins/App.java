612cd20e6887861aa3fbbfe7a8ac1278f21034c1
JulienCasta <juliencasta16@gmail.com>
2025-09-24 14:16:34 +0200

    added bad code

--- src/main/java/fr/formation/jenkins/App.java
+++ src/main/java/fr/formation/jenkins/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Integer myInt = null;
+        Integer yourInt = myInt * 2;
     }
 }
